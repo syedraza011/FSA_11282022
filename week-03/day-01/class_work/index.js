e6c39a7bfb89958cb53011c43aa3133c64ba2a51
Syed Raza <syed.raza011@gmail.com>
2022-12-12 17:58:58 -0500

    test

--- week-03/day-01/class_work/index.js
+++ week-03/day-01/class_work/index.js
@@ -18,13 +18,54 @@ to see hidden ls -al it will show any hidden folders
 //         }
 //     }
 // }
-console.log(merge(["math", "history", "physics"], ["B-", "A+", "B+"]))
-function merge(arr1, arr2) {
-    let obj = {};
-    for (let i = 0; i < arr1.length; i++) {
-        let key=arr1[i]
-      obj [key]= arr2[i];
-        //obj+=1;
+// console.log(merge(["math", "history", "physics"], ["B-", "A+", "B+"]))
+// function merge(arr1, arr2) {
+//     let obj = {};
+//     for (let i = 0; i < arr1.length; i++) {
+//         let key=arr1[i]
+//       obj [key]= arr2[i];
+//         //obj+=1;
+//     }
+//     return obj;
+//   }
+
+  //------------------
+  const cart = [
+    {
+      name: "Blue Paint",
+      quantity: 10,
+      price: 4.99,
+    },
+    {
+      name: "Red Paint",
+      quantity: 5,
+      price: 4.99,
+    },
+    {
+      name: "Yellow Paint",
+      quantity: 8,
+      price: 4.99,
+    },
+    {
+      name: "16x20in Canvas",
+      quantity: 2,
+      price: 2.99,
+    },
+    {
+      name: "Paintbrush",
+      quantity: 15,
+      price: 1.5,
+    },
+  ];
+
+  console.log(checkout(cart));
+  function checkout(obj) {
+    let sum=0;
+    for (let key in obj) {
+      console.log(key);
+    sum += obj[key].price*obj[key].quantity;
+    console.log(obj[key].price);
     }
-    return obj;
+    return sum;
   }
+  //------------
