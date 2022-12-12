c2b416e25283777250f372e75e41a4037fe2cd60
Syed Raza <syed.raza011@gmail.com>
2022-12-12 12:07:55 -0500

    Question

--- week-03/day-01/class_work/index.js
+++ week-03/day-01/class_work/index.js
@@ -3,5 +3,8 @@
 
 pwd- directory or address
 ~- means home folder
-
+mkdri fullstack
+creating a file write touvh 
+how to find in git hwere are you in git
+to see hidden ls -al it will show any hidden folders
 */
