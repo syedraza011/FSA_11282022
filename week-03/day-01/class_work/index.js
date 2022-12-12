13f01a29fcf5183db1d5f51f08bb385feab6e09b
Syed Raza <syed.raza011@gmail.com>
2022-12-12 17:12:40 -0500

    test update

--- week-03/day-01/class_work/index.js
+++ week-03/day-01/class_work/index.js
@@ -8,3 +8,23 @@ creating a file write touvh
 how to find in git hwere are you in git
 to see hidden ls -al it will show any hidden folders
 */
+// function findHerbivores(obj){
+//     let animal=[];
+//     for(let key in obj){
+//         let animal=obj[key];
+//         console.log()
+//         if(key.isHerbivore){
+//             animal.push()
+//         }
+//     }
+// }
+console.log(merge(["math", "history", "physics"], ["B-", "A+", "B+"]))
+function merge(arr1, arr2) {
+    let obj = {};
+    for (let i = 0; i < arr1.length; i++) {
+        let key=arr1[i]
+      obj [key]= arr2[i];
+        //obj+=1;
+    }
+    return obj;
+  }
