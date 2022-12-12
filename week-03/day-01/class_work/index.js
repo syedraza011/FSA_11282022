9ec923c554643260f329a81e859bfce383e90d7e
Syed Raza <syed.raza011@gmail.com>
2022-12-12 10:14:45 -0500

    Week 3 class work

--- week-03/day-01/class_work/index.js
+++ week-03/day-01/class_work/index.js
@@ -1,17 +1,7 @@
 // //Class work
-// console.log("hello");
-// document.body.querySelector(".result").innerHTML=13;
-// //string- "hello world";
-// let num1 = 13;
-// let num2 = 35;
+/*
 
-// let message = prompt("who are you saying Hello : ");
-// let concatItem= " Hello " + message;
-// // let num3 = promt("WHat is your Second number: ");
-// // document.body.querySelector(".result").innerHTML=num+num3;
-// document.body.querySelector("#result").innerHTML=concatItem;
-// // document.body.querySelector(".result").innerHTML=num+num3;
-document.body.querySelector("#result").innerHTML=2**3;
-document.body.querySelector("#result").innerHTML=2**3;
-document.body.querySelector("#result").innerHTML=13%3;
-document.body.querySelector("#result").innerHTML=15*5;
+pwd- directory or address
+~- means home folder
+
+*/
