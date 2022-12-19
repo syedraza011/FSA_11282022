470ff4720bcce25918e85eeb99f77bb9d6e0a932
Syed Raza <syed.raza011@gmail.com>
2022-12-18 21:39:30 -0500

    updated folders

--- week-04/day-01/class_work/index.js
+++ week-04/day-01/class_work/index.js
@@ -1,1 +1,22 @@
 //alert("hello");
+let btn = document.body.querySelector(".btn");
+let grandparent = document.body.querySelector(".grandParent");
+let parent = document.body.querySelector(".grandParent");
+console.log(parent);
+let children = document.querySelectorAll(".child");
+
+console.log(children);
+btn.addEventListener("click", function () {
+  for (let i = 0; i < children.length; i++) {
+    let child = parent.children[2];
+    console.log(child[0]);
+  }
+  removeColor(child[0]);
+});
+
+function changeColor(container) {
+  container.style.backgroundColor = "Green";
+}
+function removeColor(item) {
+  item.style.backgroundColor = "gray";
+}
