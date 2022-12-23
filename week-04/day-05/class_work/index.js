c674644a3ac55471c036b580b35535a868336100
Syed Raza <syed.raza011@gmail.com>
2022-12-23 16:47:01 -0500

    update

--- week-04/day-05/class_work/index.js
+++ week-04/day-05/class_work/index.js
@@ -1,7 +1,7 @@
-//alert("hello");
+alert("hello");
 // let x=10.6;
 // console.log(x);
-// const ream = {
+// const ream = {ws
 //     color: "pink",
 //     weight: 110,
 //     size: "A3",
@@ -12,7 +12,7 @@
 
 //   console.log(ream.weight)
 //   console.log(ream["weight"])
-//  // model c  
+//  // model c
 //   function createReam(color, weight, size) {
 //     return {
 //       color: color,
@@ -23,13 +23,12 @@
 //       },
 //     };
 //   }
-  
+
 //   const ream1 = createReam("white", 150, "A5");
 //   const ream2 = createReam("beige", 80, "A10");
 //   console.log(ream1.color)
-  
 
-  let x = 5;
+let x = 5;
 const foo = 10;
 
 function add(x, y) {
@@ -45,9 +44,8 @@ function multiply(x, y) {
 }
 
 let y = 8;
-console.log(multiply(3,4))
+console.log(multiply(3, 4));
 console.log(add(3, 4));
 multiply(3, 4);
 console.log(x, y);
-//console.log(bar);
-
+console.log(bar);
