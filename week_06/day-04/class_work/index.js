8c4243472088e634fbcdd6e1a1a637cd275c1a85
Syed Raza <syed.raza011@gmail.com>
2023-01-12 17:16:38 -0500

    update

--- week_06/day-04/class_work/index.js
+++ week_06/day-04/class_work/index.js
@@ -1,1 +1,63 @@
-// alert("hello");
+// // alert("hello");
+// let clicked = false;
+// let flag= document.getElementById('.sub').addEventListener("click", function() {
+//    clicked = true
+
+// });
+
+// if(clicked){
+//     // <input id="comitted" >Hello</input>
+// }
+// let container = document.querySelector(".sub");
+// console.log(container);
+// const commit_Btn = document.querySelector(".btn");
+// console.log(commit_Btn);
+// let input = document.querySelector("#input");
+// console.log(input);
+// commit_Btn.addEventListener("click", function (event) {
+//   event.preventDefault();
+
+//   container.appendChild(input);
+// // });
+// let author_header = "";
+// let btn = document.querySelector("#butn");
+// let x = document.getElementById("author");
+// btn.addEventListener("click", function (e) {
+// e.preventDefault()
+//   //classList.add name if clicked
+//   //class of hidden
+//   //in css display none  by difault
+//   // clicking event listner , classlist.remove from auhtor
+//   //
+//   author_header = x.value;
+//   console.log(author_header);
+//   return author;
+// });
+// const body=document.querySelector("body");
+// const container = document.createElement("div");
+// body.append(container);
+// // console.log(x.);
+
+// const author = document.createElement("h3");
+// console.log(author_header);
+// container.append(author);
+
+// author.innerText = author_header;
+// console.log(author)
+
+// // console.log(author)
+// // container.innerHTML = x.value;
+// // console.log(author.value);
+
+// // console.log(container);
+// container.append(author_header);
+let btn=document.querySelector("#butn");
+let input=document.querySelector("#author");
+let body=document.querySelector("body");
+let title=document.createElement("div");
+title.setAttribute("id","divSubmit");
+body.append(title);
+btn.addEventListener("click",function(e){
+e.preventDefault();
+title.innerHTML=input.value;
+})
