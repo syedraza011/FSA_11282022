36e89dd86df3e661a255cd6e5f8acf75b2defa9c
Syed Raza <syed.raza011@gmail.com>
2023-01-23 10:45:06 -0500

    api

--- week_08/week-08_day-01/index.js
+++ week_08/week-08_day-01/index.js
@@ -1,2 +1,10 @@
-alert("hello");
+// alert("hello");
 
+//npm install express
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1> Hello world</h1>");
+});
+app.listen(3000);
